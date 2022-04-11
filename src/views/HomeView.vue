ef5cf5064db5541b5bca71873eeff3dc531995b7
yoon <11nux@kakao.com>
2022-04-11 17:24:18 +0900

    투두 리스트 컴포넌트 따로 분리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <li v-for="todo in todos" v-bind:key="todo">
-      {{ todo.text }}
-    </li>
-    <todo-item v-for="item in todos" v-bind:todo="item" v-bind:key="item.text">
-    </todo-item>
+    <TodoList v-for="todo in todos" v-bind:key="todo.text" v-bind:todo="todo" />
   </div>
 </template>
 
 <script lang="ts">
 import mockup from '@/store/mockup';
-import Vue from 'vue';
-
-// 화면에서 표시 안되는 문제 있음
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>',
-});
+import TodoList from '@/components/todo-list.vue';
 
 export default {
-  components: {},
+  components: { TodoList },
   data: function () {
     return {
       todos: mockup,

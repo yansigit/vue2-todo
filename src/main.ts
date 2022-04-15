35107ef63a30aa185177ab3a10160388d84d121c
yoon <11nux@kakao.com>
2022-04-15 15:43:09 +0900

    chore: vue, vuex, vue-router 버전에 맞춰 코드 조정

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.config.productionTip = false;
+// createApp(App).use(store).use(router).mount('#app');
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
+
+// const app = createApp({
+//   router,
+//   store,
+//   render: (h) => h(App),
+// }).mount('#app');
